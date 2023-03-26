1ae9648ec4a04bf5bd6f74a0969538aeb4bb4cf1
tcyang1971 <tcyang@gm.pu.edu.tw>
2023-03-26 15:38:30 +0800

    v.2文字效果

--- app/src/main/java/tw/edu/pu/tcyang/gesture/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/tcyang/gesture/MainActivity.kt
@@ -1,11 +1,24 @@
 package tw.edu.pu.tcyang.gesture
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var txv: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txv = findViewById(R.id.txv)
+        txv.setTextColor(Color.parseColor("#eeee00"))
+        txv.setBackgroundColor(Color.BLUE)
+        txv.setTypeface(
+            Typeface.createFromAsset(assets,
+                "font/HanyiSentyFingerPainting.ttf"))
+        txv.getBackground().setAlpha(50)  //0~255透明度值
     }
 }
